6654c184c72a6aa14d0374d4e17c68d7c778eead
Matthijs <mevanloon@me.com>
2017-10-03 01:27:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZenScribe
+
+ZenScribe is an easy text editor. The editor should not get in your way. The editor should allow you to write. Easily.
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Textarea/>
+      </div>
+    );
+  }
+}
+
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+  element.style.height = window.screen.availHeight;
+  console.log(element.style);
+}
+
+class Textarea extends Component {
+  doFocus() {
+    this.textarea.focus();
+  }
+  componentDidMount() {
+    this.doFocus();
+  }
+  onKeyUp(e) {
+    if(e.keyCode === 13)
+      console.log(e.target.innerText);
+  }
+  render() {
+    return (
+      <div className="textarea"  autoFocus tabIndex="0" contentEditable="true" onDoubleClick={(e)=>{launchIntoFullscreen(document.getElementById('#root'))}} onKeyUp={this.onKeyUp} ref={(input) => this.textarea = input}>
+      </div>
+    )
+  }
+}
+
+export default App;
